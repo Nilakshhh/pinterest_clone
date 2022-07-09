44ceba0a8e53a0f8a38c390ca0539c207d0976d6
Nilakshhh <97288540+Nilakshhh@users.noreply.github.com>
2022-07-09 16:51:29 +0530

    adding functionality to site

---
+++ public/scripts/index.js
@@ -0,0 +1,26 @@
+
+
+
+document.querySelector("#create").addEventListener('click', () => {
+    var element = document.getElementById("add_pin_container");
+  element.classList.remove("hidden");
+  var element = document.getElementById("pin_container");
+  element.classList.add("hidden");
+
+
+
+
+
+});
+
+
+
+
+
+
+document.querySelector(".save_pin").addEventListener('click', () => {
+    var element = document.getElementById("add_pin_container");
+  element.classList.add("hidden");
+  var element = document.getElementById("pin_container");
+  element.classList.remove("hidden");
+});
